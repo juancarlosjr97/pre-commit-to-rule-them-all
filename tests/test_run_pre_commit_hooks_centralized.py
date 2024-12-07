b68224d5e04b47e1d5aac1a2d880f9af7bedaa57
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2024-12-07 16:26:07 +0000

    feat: add pre-commit for rust
    #11

---
+++ tests/test_run_pre_commit_hooks_centralized.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Test module for the pre_commit_to_rule_them_all module
+"""
+import unittest
+from unittest.mock import patch
+
+from pre_commit_to_rule_them_all import run_pre_commit_hooks_centralized
+
+
+class TestYamlConfigMethods(unittest.TestCase):
+    """Tests for each of the functions, whether files exist or not."""
+
+    def test_invalid_file_name(self):
+        """
+        Test a path the doesn't exist
+        """
+        self.assertRaises(
+            FileNotFoundError,
+            run_pre_commit_hooks_centralized.execute_pre_commit_hooks_centralized, "beans")
+
+    @patch('pre_commit_to_rule_them_all.run_pre_commit_hooks_centralized.os')
+    def test_use_pre_commit_hooks_rust(self, os_mock):
+        """
+        Test a valid path for rust pre-commit
+        """
+        os_mock.path.isdir.return_value = False
+
+        run_pre_commit_hooks_centralized.use_pre_commit_hooks_rust()
+
+if __name__ == '__main__':
+    unittest.main()
